b394b01f95417cc4788f3d27ca2e25210f5573d0
becks256 <22892728+becks256@users.noreply.github.com>
2022-10-12 13:53:54 -0400

    feat: split string arguments on whitespace and so we can dedupe every class name passed.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 export function clsnx() {
   const classes = []
   Array.from(arguments).forEach((item) => {
-    typeof item === "string" && classes.push(item)
-    if (typeof item === "object") {
+    if (typeof item === "string") {
+      item.split(/\s/).forEach(classItem => {
+        classes.push(classItem)
+      })
+    } else if (typeof item === "object") {
       Object.keys(item).forEach((i) => {
         if (typeof item[i] === "boolean") {
           item[i] && classes.push(i)
         } else if (typeof item[i] === "string") {
           classes.push(item[i])
         } else if (typeof item[i] === "object") {
-          item[i] && classes.push(classname(item[i]))
+          item[i] && classes.push(clsnx(item[i]))
         }
       })
     }
